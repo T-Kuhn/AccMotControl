6d56da2a02080dbfe810772b3f7af7d40afc9d33
Tobias Kuhn <tkuhn.mpu@gmail.com>
2016-01-05 16:11:04 +0900

    implemented linear slope at start / stop

--- AccMotControl.cpp
+++ AccMotControl.cpp
@@ -30,28 +30,32 @@ void AccMotControl::begin()
     pinMode(_motorDriverIN1pin, OUTPUT);
     pinMode(_motorDriverIN2pin, OUTPUT);
     mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
-    mpu.setDLPFMode(MPU6050_DLPF_5);   
-    pid.begin(250.0f, 0.00f, 150.0f);
+    mpu.setDLPFMode(MPU6050_DLPF_4);   
+    _pathFollowing = false;
+    pid.begin(200.0f, 0.0f, 0.0f);
     pid.setSetPoint(4.71f);
 }
 
+
 // - - - - - - - - - - - - - - - - - - -
-// - - - AccMotControl UPDATE  - - - - -
+// - - - - AccMotControl UPDATE  - - - -
 // - - - - - - - - - - - - - - - - - - -
-void AccMotControl::updatePID()
+void AccMotControl::update()
 {
-    _cntr++;
-    if(_cntr % 1000 == 0){
-        pid.setSetPoint(0.78f);
+    if(_pathFollowing){
+        _followPath();
     }
-    if(_cntr % 2000 == 0){
-        pid.setSetPoint(2.36f);
-    }
-    if(_cntr % 2999 == 0 || _cntr >= 3000){
-        pid.setSetPoint(5.50f); 
-        _cntr = 1;
-    }
-    Serial.println(_getRotAngle());
+    _updatePID();
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - - AccMotControl UPDATE PID  - - - -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_updatePID()
+{
+    //_setPointSetter();
+    _moveFunctionCaller();
+    //Serial.println(_getRotAngle());
     analogWrite(_motorDriverPWMpin, _setRotDir(pid.update(_getRotAngle())));
 }
 
@@ -96,5 +100,120 @@ float AccMotControl::_getRotAngle()
     return 0.0f;
 }
 
+// - - - - - - - - - - - - - - - - - - -
+// - - - - AccMotControl MOVE  - - - - -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::move(float goalPos, unsigned int moveTime, unsigned int moveSlopeTime)
+{
+    _pathFollowing = true;
+    _goalPos = goalPos;
+    _moveTime = moveTime;
+    _moveSlopeTime = moveSlopeTime;
+    _startPos = _getRotAngle();
+    _startTime = millis();
+    _calculatePathVars();
+}
 
+// - - - - - - - - - - - - - - - - - - -
+// - AccMotControl CALCULATE PATHVARS  -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_calculatePathVars()
+{
+    _distance = _goalPos - _startPos; // can be minus
+    if(2 * _moveSlopeTime < _moveTime){
+        _moveStraightTime = _moveTime - 2 * _moveSlopeTime;  
+    }else{
+        _moveStraightTime = 0.0f;
+    }
+    _maxSpeed = _distance / (_moveSlopeTime + _moveStraightTime);
+    _slope = _maxSpeed / _moveSlopeTime;
+    
+    _startSlopeEndPos = _maxSpeed / 2 * _moveSlopeTime;
+    _straightMoveEndPos = _startSlopeEndPos + _maxSpeed * _moveStraightTime;
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - - - AccMotControl FOLLOW PATH - - -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_followPath()
+{
+    _currentTime = millis();
+    _passedTime = _currentTime - _startTime;
+    if(_moveSlopeTime >= _passedTime){
+        _followStartSlope();
+        return;
+    }else if(_moveSlopeTime + _moveStraightTime >= _passedTime){
+        _followStraightLine();
+        return;
+    }else if(2 * _moveSlopeTime + _moveStraightTime >= _passedTime){
+        _followEndSlope();
+        return;
+    }else{
+        pid.setSetPoint(_goalPos);
+        _pathFollowing = false; // in the END...
+    }
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - AccMotControl FOLLOW START SLOPE  -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_followStartSlope()
+{
+    pid.setSetPoint(_startPos + _passedTime * _passedTime * _slope / 2);
+}
+
+// - - - - - - - - - - - - - - - - - - -
+//  AccMotControl FOLLOW STRAIGHT LINE -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_followStraightLine()
+{
+    pid.setSetPoint(_startPos + _startSlopeEndPos + (_passedTime - _moveSlopeTime) * _maxSpeed);    
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - AccMotControl FOLLOW END SLOPE  - -
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_followEndSlope()
+{
+    unsigned int passedTimeSinceStartEndSlope = _passedTime - _moveSlopeTime - _moveStraightTime;
+    pid.setSetPoint(_startPos + _straightMoveEndPos +
+            (_maxSpeed - _slope / 2 * passedTimeSinceStartEndSlope) * 
+            passedTimeSinceStartEndSlope); 
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - - - SET POINT SETTER [DEBUG]  - - -  
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_setPointSetter()
+{
+    _cntr++;
+    if(_cntr % 1000 == 0){
+        pid.setSetPoint(0.78f);
+    }
+    if(_cntr % 2000 == 0){
+        pid.setSetPoint(2.36f);
+    }
+    if(_cntr % 2999 == 0 || _cntr >= 3000){
+        pid.setSetPoint(5.50f); 
+        _cntr = 1;
+    }
+}
+
+// - - - - - - - - - - - - - - - - - - -
+// - - MOVE FUNCTION CALLER [DEBUG]  - -  
+// - - - - - - - - - - - - - - - - - - -
+void AccMotControl::_moveFunctionCaller()
+{
+    _cntr++;
+    if(_cntr % 2000 == 0){
+        move(0.78f, 1000, 200);
+    }
+    if(_cntr % 4000 == 0){
+        move(2.36f, 500, 200);
+    }
+    if(_cntr % 5999 == 0 || _cntr >= 6000){
+        move(5.50f, 650, 200); 
+        _cntr = 1;
+    }
+}
 
--- AccMotControl.h
+++ AccMotControl.h
@@ -21,14 +21,37 @@ class AccMotControl
         MPU6050 mpu;
         PID pid;
         void begin();
-        void updatePID();
+        void update();
+        void move(float goalPos, unsigned int moveTime, unsigned int moveSlopeTime);
     private:
+        void _updatePID();
+        int _setRotDir(int val);
+        float _getRotAngle();
+        void _calculatePathVars();
+        void _followPath();
+        void _followStartSlope();
+        void _followStraightLine();
+        void _followEndSlope();
+        void _setPointSetter();
+        void _moveFunctionCaller();
         int _motorDriverIN1pin;
         int _motorDriverIN2pin;
         int _motorDriverPWMpin; 
-        int _setRotDir(int val);
-        float _getRotAngle();
         int _cntr;
+        bool _pathFollowing;
+        float _goalPos;
+        float _startPos;
+        unsigned long _startTime;
+        unsigned long _currentTime;
+        unsigned int _passedTime;
+        unsigned int _moveTime;
+        unsigned int _moveSlopeTime;
+        unsigned int _moveStraightTime;
+        float _distance;
+        float _maxSpeed;
+        float _slope;
+        float _startSlopeEndPos;
+        float _straightMoveEndPos;
 };
 
 #endif
